17b24be794e9f3a2ced08131329cc1a634b908b8
anishchapagain <anishchapagain>
2019-05-20 18:00:13 +0545

    sd

--- Quotes/settings.py
+++ Quotes/settings.py
@@ -19,12 +19,12 @@ NEWSPIDER_MODULE = 'Quotes.spiders'
 #USER_AGENT = 'Blog (+http://www.yourdomain.com)'
 #ROBOTSTXT_OBEY = False
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS=16
+#CONCURRENT_REQUESTS=16
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY=3
+#DOWNLOAD_DELAY=3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
@@ -64,25 +64,25 @@ DOWNLOAD_DELAY=3
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-    'Quotes.pipelines.QuotesPipeline': 300,
-}
-FEED_EXPORT_ENCODING = 'utf-8'
+#ITEM_PIPELINES = {
+#    'Quotes.pipelines.QuotesPipeline': 300,
+#}
+#FEED_EXPORT_ENCODING = 'utf-8'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 # NOTE: AutoThrottle will honour the standard settings for concurrency and delay
-AUTOTHROTTLE_ENABLED=True
+#AUTOTHROTTLE_ENABLED=True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY=5
+#AUTOTHROTTLE_START_DELAY=5
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY=60
+#AUTOTHROTTLE_MAX_DELAY=60
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG=False
-DOWNLOAD_HANDLERS = {'s3': None,}
+#DOWNLOAD_HANDLERS = {'s3': None,}
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED=True
+#HTTPCACHE_ENABLED=True
 #HTTPCACHE_EXPIRATION_SECS=0
 #HTTPCACHE_DIR='httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES=[]
